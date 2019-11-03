dcf0d356c5a943e18faca97f04eca1696efab09d
Anmol Takiar <34587426+atakiar@users.noreply.github.com>
2019-11-03 01:06:39 -0400

    move word length correction to spell checking

--- server/services/textProcessing/wordLengthCorrection.js
+++
@@ -1,8 +0,0 @@
-const wordLengthCorrection = text => {
-  let result = text;
-  result = result.replace(/-\n/g, '');
-  result = result.replace(/\n/g, ' ');
-  return result;
-};
-
-module.exports = wordLengthCorrection;
