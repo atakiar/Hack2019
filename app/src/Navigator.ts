90e899b506171ec9e8b55dec6194545bc70337f7
Anmol Takiar <34587426+atakiar@users.noreply.github.com>
2019-11-03 05:43:58 -0500

    wow

--- app/src/Navigator.ts
+++ app/src/Navigator.ts
@@ -1,31 +1,63 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import ReadScreen from './screens/Read';
-import SettingsScreen from './screens/Settings';
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
+import HistoryScreen from "./screens/History";
+import ReadScreen from "./screens/Read";
+import SettingsScreen from "./screens/Settings";
 
-const AppNavigator = createStackNavigator(
+const AppNavigator = createBottomTabNavigator(
   {
+    History: {
+      screen: ReadScreen,
+      navigationOptions: ({ navigation }) => {
+        return {
+          title: "History"
+        };
+      }
+    },
     Read: {
       screen: ReadScreen,
       navigationOptions: ({ navigation }) => {
-        console.log(navigation)
-        return ({
-          title: 'Read',
-        })
-      },
+        return {
+          title: "Lumen"
+        };
+      }
     },
     Settings: {
       screen: SettingsScreen,
-      navigationOptions: {
-        title: 'Options',
-      },
-    },
+      navigationOptions: ({ navigation }) => {
+        return {
+          title: "Settings"
+        };
+      }
+    }
   },
   {
-    initialRouteName: 'Read',
-  },
-);
+    initialRouteName: "Read",
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
 
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          IconComponent = HomeIconWithBadge;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <MaterialIcons name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
 
 export default createAppContainer(AppNavigator);
